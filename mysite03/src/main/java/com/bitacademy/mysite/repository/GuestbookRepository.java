374c6b9f8549c9044f3b165ba1fe4dcd137d00c4
leejuno <zhqkfxm0127@naver.com>
2023-07-20 14:57:53 +0900

    Add: mystie03 - mybatis I
    mybatis 설정들 + select, insert에 적용

--- mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
@@ -8,6 +8,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bitacademy.mysite.vo.GuestbookVo;
@@ -15,110 +19,17 @@ import com.bitacademy.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
-	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<>();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			// 1. 클래스로딩
-			Class.forName("org.mariadb.jdbc.Driver");
-			// 2. 연결
-			String url = "jdbc:mariadb://192.168.0.150:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			// 3. statement준비
-			String sql =" select no, name, password, message, reg_date "
-					+ "     from guestbook"
-					+ " order by no desc";
-			pstmt = conn.prepareStatement(sql);
-			// 4. 바인딩 
-			
-			// 5. statement실행
-			rs = pstmt.executeQuery();
-			
-			// 5. 결과처리
-			while(rs.next()) {
-				Long no = rs.getLong(1); // DB는 1부터시작
-				String name = rs.getString(2);
-				String password = rs.getString(3);
-				String message = rs.getString(4);
-				String regDate = rs.getString(5);
-				
-				GuestbookVo vo = new GuestbookVo();
-				vo.setNo(no);
-				vo.setName(name);
-				vo.setPassword(password);
-				vo.setMessage(message);
-				vo.setRegDate(regDate);
-				
-				result.add(vo);
-			}
-			System.out.println("select 성공");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: "+ e);
-		} catch (SQLException e) {
-			System.out.println("Error: "+ e);
-		} finally {// 6. 자원정리
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+	@Autowired
+	private DataSource dateSource;
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<GuestbookVo> findAll() {// namespace.id
+		return sqlSession.selectList("guestbook.findAll");
 	}
 	
-	public void insert(GuestbookVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			// 1. 클래스로딩
-			Class.forName("org.mariadb.jdbc.Driver");
-			// 2. 연결
-			String url = "jdbc:mariadb://192.168.0.150:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			// 3. statement준비
-			String sql ="insert into guestbook "
-					+ "       values (null, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-			// 4. 바인딩 
-			pstmt.setString(1,vo.getName());
-			pstmt.setString(2,vo.getPassword());
-			pstmt.setString(3,vo.getMessage());
-			
-			// 5. statement실행
-			rs = pstmt.executeQuery();
-			
-			// 6. 결과처리할게 있나 -> sql문 실행하면 끝인데
-			System.out.println("insert성공");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: "+ e);
-		} catch (SQLException e) {
-			System.out.println("Error: "+ e);
-		} finally {// 6. 자원정리
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+	public boolean insert(GuestbookVo vo) {
+		return 1 == sqlSession.insert("guestbook.insert");
 	}
 	
 	public void deleteByNoAndPassword(Long no, String password) {
